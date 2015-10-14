99734f99e7e3df1cf918cf172e73c97f73e05e46
LiYChristopher <LiYChristopher92@gmail.com>
2015-10-14 15:18:30 -0400

    Update the_pawn_brotherhood.py

--- the_pawn_brotherhood.py
+++ the_pawn_brotherhood.py
@@ -1,24 +1,13 @@
 '''
-Chess is a two-player strategy game played on a checkered game board laid out in eight rows 
-(called ranks and denoted with numbers 1 to 8) and eight columns (called files and denoted with letters a to h) of squares. 
-Each square of the chessboard is identified by a unique coordinate pair
-— a letter and a number (ex, "a1", "h8", "d6"). 
-For this mission we only need to concern ourselves with pawns. A pawn may capture an opponent's piece 
-on a square diagonally in front of it on an adjacent file, by moving to that square. 
-For white pawns the front squares are squares with greater row than their.
-
-A pawn is generally a weak unit, but we have 8 of them which we can use to build a pawn defense wall. 
-With this strategy, one pawn defends the others. 
-A pawn is safe if another pawn can capture a unit on that square. 
-We have several white pawns on the chess board and only white pawns. 
-You should design your code to find how many pawns are safe.
+From the Checkio task page:
+"For a game AI one of the important tasks is the ability to estimate game state. 
+This concept will show how you can do this on the simple chess figures positions."
 
 Input: Placed pawns coordinates as a set of strings.
 
 Output: The number of safe pawns as a integer.
 
-For a game AI one of the important tasks is the ability to estimate game state. 
-This concept will show how you can do this on the simple chess figures positions.
+More info at https://www.checkio.org/mission/pawn-brotherhood/
 '''
 
 def board():
