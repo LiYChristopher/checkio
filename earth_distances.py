9c80b0d0107e120f49d6f1fca461229f6e014a08
LiYChristopher <LiYChristopher92@gmail.com>
2015-10-27 22:35:48 -0400

    Update earth_distances.py

--- earth_distances.py
+++ earth_distances.py
@@ -1,3 +1,14 @@
+'''
+IN - Two pairs of Lat./Long. coordinates (First --> Second)
+OUT- distance between the two points in kilometers
+
+"The concepts presented in this mission are the exact sorts of concepts used in navigational software, 
+enabling a ship or plane to understand where it is, where it must go and how far it has gone. 
+Along the same vein, Global Positioning Satellites use similar principles to provide 
+pinpoint accurate locations to GPS receivers for use in navigation."
+
+Excerpt taken from CheckIO mission page. Visit http://www.checkio.org/mission/earth-distances/ for more information.
+'''
 import re
 from math import radians
 from math import pi, sqrt
