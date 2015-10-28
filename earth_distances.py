f722eb7db652355a240a51cf0693e9b374b0e486
LiYChristopher <LiYChristopher92@gmail.com>
2015-10-27 21:36:59 -0400

    Create earth_distances.py

---
+++ earth_distances.py
@@ -0,0 +1,55 @@
+import re
+from math import radians
+from math import pi, sqrt
+from math import sin, cos, atan2
+R = 6371
+​
+def clean_input(input_string):
+    ''' Takes pairs of coordinates (first or second),
+    and returns a namedtuple-object to be used in the
+    main function. '''
+​
+    format = re.sub(r'(,\s)|\,', " ",input_string, re.I)
+    pair_points = format.encode('utf-8').split(' ')
+    clean = lambda point: (re.search(r'([0-9]*)..([0-9]*)...([0-9]*)...([A-Z]*)',
+                                    point, re.I)).groups()
+    result = [clean(point) for point in pair_points]
+    return result
+​
+def convert_to_radians(deg, min, sec, dir):
+    ''' Calculates total degrees of a coordinate (Lat or Long),
+    then converts it into radians.'''
+    
+    total_degrees = float(deg) + (float(min)/60) + (float(sec)/60**2)
+    if dir == "S" or dir == "W":
+        total_degrees *= -1
+    total_radians = radians(total_degrees)
+    return total_radians
+    
+def distance(first, second):
+    ''' Calculates shortest-distance over the Earth's surface 
+    based on haversine formula:
+    a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+    c = 2 ⋅ atan2( √a, √(1−a) )
+    d = R ⋅ c
+    '''
+    
+    lat1, long1 = clean_input(first)
+    lat2, long2 = clean_input(second)
+​
+    phi1 = convert_to_radians(*lat1)
+    phi2 = convert_to_radians(*lat2)
+    d1 = convert_to_radians(*long1)
+    d2 = convert_to_radians(*long2)
+    
+    d_phi = phi2 - phi1
+    d_lambda = d2 - d1
+    
+    a = sin(d_phi/2)**2 +\
+        cos(phi1) * cos(phi2) *\
+        sin(d_lambda/2)**2 
+        
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    
+    distance = R * c
+    return distance
